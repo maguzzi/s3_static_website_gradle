74df203d301e86eb1a1b3c0fab943b4f96a844e5
marco.aguzzi@gmail.com <marco.aguzzi@gmail.com>
2024-02-18 11:08:22 +0100

    stack filtered by tag

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,13 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
 import software.amazon.awssdk.services.cloudformation.model.*;
 
-import software.amazon.awssdk.services.cloudformation.model.Tag;
-import software.amazon.awssdk.services.cloudformation.model.Parameter;
-
-import java.net.URI;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import java.nio.file.Path;
 import java.nio.file.Files;
 
@@ -16,7 +18,12 @@ import java.nio.file.Files;
 public class App {
 
     private static final Region region = Region.US_EAST_1;
+
     private static final String BOOTSTRAP_STACK_NAME = "s3-static-website-bootstrap-stack";
+
+    private static final String S3_STATIC_WEBSITE_ENVIRONMENT_TAG="s3_static_website_environment";
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     private static CloudFormationClient cloudFormationClient = CloudFormationClient.builder().region(region).build();
 
@@ -27,7 +34,7 @@ public class App {
         // TODO file inside the jar
         Path templatePath = Paths.get("./src/main/resources/bootstrap/bootstrap.json");
 
-        log("reading template from: "+templatePath.toAbsolutePath());
+        logger.info("reading template from: {}",templatePath.toAbsolutePath());
 
         String templateBody = new String(Files.readAllBytes(templatePath));
 
@@ -45,7 +52,35 @@ public class App {
             .build();
 
         CreateStackResponse response = cloudFormationClient.createStack(request);
+        
+        screenMessage("BOOSTRAP STACK CREATION END");
+    }
 
+    public void createDistribution(String environmentString) throws Exception {
+        screenMessage("BOOSTRAP STACK CREATION START");
+
+        // TODO file inside the jar
+        Path templatePath = Paths.get("./src/main/resources/bootstrap/bootstrap.json");
+
+        logger.debug("reading template from: {}",templatePath.toAbsolutePath());
+
+        String templateBody = new String(Files.readAllBytes(templatePath));
+
+        Parameter environment = Parameter
+            .builder()
+            .parameterKey("Environment")
+            .parameterValue(environmentString)
+            .build();
+
+        CreateStackRequest request = CreateStackRequest.builder()
+            .stackName(BOOTSTRAP_STACK_NAME+"-"+environmentString)
+            .templateBody(templateBody)
+            .parameters(environment)
+            .tags(Tag.builder().key(S3_STATIC_WEBSITE_ENVIRONMENT_TAG).value(environmentString).build())
+            .build();
+
+        CreateStackResponse response = cloudFormationClient.createStack(request);
+        
         screenMessage("BOOSTRAP STACK CREATION END");
     }
 
@@ -59,34 +94,35 @@ public class App {
                 .region(region)
                 .build();
 
-        ListStacksResponse listStacksResponse = cloudFormationClient.listStacks();
+        ListStacksResponse listStacksResponse = cloudFormationClient.listStacks(
+        ListStacksRequest.builder()
+            .stackStatusFiltersWithStrings("CREATE_COMPLETE","UPDATE_COMPLETE")
+            .build()
+        );
 
         for (StackSummary stackSummary : listStacksResponse.stackSummaries()) {
-            if (stackSummary.stackName().contains("dev") && !(stackSummary.stackStatus().toString().equals("DELETE_COMPLETE"))) {
-                System.out.println(stackSummary.stackName()+" ("+stackSummary.stackStatus()+")" );
-            }
+            logger.trace("Checking stack {}",stackSummary.stackName());
+            DescribeStacksRequest describeStacksRequest = DescribeStacksRequest.builder().stackName(stackSummary.stackName()).build();
+            DescribeStacksResponse describeStacksResponse = cloudFormationClient.describeStacks(describeStacksRequest);    
+            List<Stack> stacks = describeStacksResponse.stacks().stream().filter(i->i.tags().stream().anyMatch(j->j.key().equals(S3_STATIC_WEBSITE_ENVIRONMENT_TAG))).collect(Collectors.toList());
+            stacks.forEach(it->logger.info("{} ({}) - {}", it.stackName(),it.stackStatusAsString(),it.tags()));
         }
 
         screenMessage("LIST STACK END");
     }
 
-    // TODO use log framework here
     private static void screenMessage(String message) {
-        System.out.println();
-        System.out.println(String.format(" -- %s --",message));
-        System.out.println();
+        logger.info("");
+        logger.info(" -- {} --",message);        
+        logger.info("");
     }
 
-    // TODO use log framework here
-    private static void log(String message) {
-        System.out.println(message);
-    }
 
     public static void main(String[] args) {
         try {
             App app = new App();
             if (args.length!=2) {
-                System.err.println("Command is required");
+                logger.error("Command is required");
             }
             String command = args[0];
             String environment = args[1];
@@ -96,8 +132,12 @@ public class App {
                     app.listStacks();
                     break;
                 }
-                case "CREATE_BOOTSTRAP": {
+                case "BOOTSTRAP": {
                     app.createBootstrap(environment);
+                    break;
+                }
+                case "DISTRIBUTION": {
+                    app.createDistribution(environment);
                     break;
                 }
 

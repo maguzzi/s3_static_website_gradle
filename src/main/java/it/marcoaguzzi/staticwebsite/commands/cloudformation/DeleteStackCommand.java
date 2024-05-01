afb1fe03e92b0f7cfdc0df4f7d0baedc04bff1b2
marco aguzzi <marco.aguzzi@gmail.com>
2024-05-01 18:50:27 +0200

    wip delete

--- src/main/java/it/marcoaguzzi/staticwebsite/commands/cloudformation/DeleteStackCommand.java
+++ src/main/java/it/marcoaguzzi/staticwebsite/commands/cloudformation/DeleteStackCommand.java
@@ -23,7 +23,75 @@ public class DeleteStackCommand extends GetRoute53InfoCommand {
 
     @Override
     public Map<String, OutputEntry> execute() throws Exception {
-        logger.warn("This command is still WIP");
-        return new HashMap<>();
+        List<String> bucketNames = Arrays.asList(
+                "s3-static-website-compiled-template-" + App.getEnvironment() + "-"
+                        + App.getPsedoRandomTimestampString(),
+                "s3-static-website-content-" + App.getEnvironment() + "-" +
+                        App.getPsedoRandomTimestampString(),
+                "s3-static-website-lambda-artifact-" + App.getEnvironment() + "-"
+                        + App.getPsedoRandomTimestampString());
+
+        // TODO strict s3 bucket control enable after more stable
+        List<String> bucketNamesToDelete = s3Client.listBuckets(ListBucketsRequest.builder().build()).buckets()
+                .stream()
+                .filter(it -> bucketNames.contains(it.name()))
+                .map(it -> it.name())
+                .collect(Collectors.toList());
+
+        bucketNamesToDelete.stream().forEach(b -> {
+            logger.info("Checking {} objects", b);
+            s3Client.listObjects(ListObjectsRequest.builder().bucket(b).build())
+                    .contents().forEach(o -> {
+                        s3Client.deleteObject(DeleteObjectRequest.builder().bucket(b).key(o.key()).build());
+                        logger.info(o.key() + " deleted");
+                    });
+        });
+
+        Optional<String> hostedZoneID = super.getHostedZoneID();
+        if (hostedZoneID.isPresent()) {
+            ListResourceRecordSetsResponse listResourceRecordSets = super.route53Client.listResourceRecordSets(
+                    ListResourceRecordSetsRequest.builder().hostedZoneId(hostedZoneID.get()).build());
+            listResourceRecordSets.resourceRecordSets()
+                    .stream()
+                    .forEach(it -> {
+
+                        if (!RRType.NS.equals(it.type()) && !RRType.SOA.equals(it.type())) {
+                            logger.warn("deleting {} - {}", it.type(), it.name());
+
+                            ChangeResourceRecordSetsRequest changeResourceRecordSetsRequest = ChangeResourceRecordSetsRequest
+                                    .builder()
+                                    .hostedZoneId(hostedZoneID.get())
+                                    .changeBatch(ChangeBatch.builder().changes(
+                                            Change
+                                                    .builder()
+                                                    .action(ChangeAction.DELETE)
+                                                    .resourceRecordSet(it).build())
+                                            .build())
+                                    .build();
+
+                            route53Client.changeResourceRecordSets(changeResourceRecordSetsRequest);
+                        } else {
+                            logger.info("leaving {} - {}", it.type(), it.name());
+                        }
+                    });
+
+        } else {
+            logger.warn("Can't find hostedZoneID, check your AWS configuration");
+        }
+
+        String distributionStackName = CommandFactory.DISTRIBUTION_STACK_NAME + "-" + App.getEnvironment();
+        logger.info("Deleting stack {}", distributionStackName);
+        cloudFormationClient.deleteStack(DeleteStackRequest.builder().stackName(distributionStackName).build());
+
+        /*
+         * String bootstrapStackName =
+         * "s3-static-website-bootstrap-stack-"+App.getEnvironment();
+         * logger.info("Deleting stack {}", bootstrapStackName);
+         * cloudFormationClient.deleteStack(DeleteStackRequest.builder().stackName(
+         * bootstrapStackName).build());
+         */
+
+        // removeLambdaFunctionAssociation(); // this seems immediate, but not enough to
+        // delete the stack return new HashMap<>();
     }
 }
